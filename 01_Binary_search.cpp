02d48351b55725e03634e71c5bba4a0f78757308
PrasanaHarshaBajracharya <prasanaharshabajracharya@gmail.com>
2025-03-15 21:30:23 +0545

    Update 01_Binary_search.cpp

--- 01_Binary_search.cpp
+++ 01_Binary_search.cpp
@@ -1,64 +1,46 @@
-# include <iostream>
-# include <vector> 
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int binarysearch(vector<int> arr, int tar){
-    int start = 0, end = arr.size()-1;
+// Binary search function
+int binarysearch(vector<int> arr, int tar) {
+    int start = 0, end = arr.size() - 1;
 
-    while (start <= end){
-        int mid = (start + end) / 2;
+    while (start <= end) {
+        int mid = start + (end - start) / 2; // Avoids potential overflow
 
-        if (tar > arr[mid]){
+        if (tar > arr[mid]) {
             start = mid + 1;
-        }
-        else if (tar < arr[mid]){
+        } else if (tar < arr[mid]) {
             end = mid - 1;
-        }
-        else {
-            return mid ;
+        } else {
+            return mid; // Target found
         }
     }
 
-    return -1 ; 
+    return -1; // Target not found
 }
-    
 
-  
-int main(){
+int main() {
+    int target;
+    cout << "Enter the element you want to search: ";
+    cin >> target;
 
-    int target ;
-    int N;
-    // target = 56 ; 
+    // Create a sorted vector from 1 to 1000
+    vector<int> arr1;
+    for (int i = 1; i <= 1000; ++i) {
+        arr1.push_back(i);
+    }
 
-    cout << "Enter the Element you want :";
-    cin >>  target ;
-     vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-                31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-                51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-                61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-                71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-                91, 92, 93, 94, 95, 96, 97, 98, 99, 100 };
-//    vector<int> arr1(N);
-//        for (int i = 0; i < N; i++) {
-//            arr1[i] = i + 1; // Sorted array from 1 to N
-//        }
+    // Perform binary search
+    int index = binarysearch(arr1, target);
 
-    int index = binarysearch(arr1 , target);
-
-    cout << "The " <<  target  << " is in index " << index ;
-
-    // cout << "The Element is in index "<< binartsearch(arr1 , target);
-
-    // vector<int> arr2 = {-1 , 0 , 3 ,  5 , 9 , 12};
-    // int tar2 = 0;
-
-
-
+    // Output the result
+    if (index != -1) {
+        cout << "The element " << target << " is at index " << index << endl;
+    } else {
+        cout << "The element " << target << " is not found in the array." << endl;
+    }
 
     return 0;
 }
-
