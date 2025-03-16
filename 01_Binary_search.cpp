239e5f28f728f4a24bc694fc72b3f623cef73b16
PrasanaHarshaBajracharya <prasanaharshabajracharya@gmail.com>
2025-03-16 08:22:18 +0545

    01_Binary_search.cpp

--- 01_Binary_search.cpp
+++ 01_Binary_search.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 // Binary search function
 int binarysearch(vector<int> arr, int tar) {
@@ -22,24 +24,33 @@ int binarysearch(vector<int> arr, int tar) {
 }
 
 int main() {
-    int target;
-    cout << "Enter the element you want to search: ";
-    cin >> target;
+    // Varying input sizes
+    vector<int> inputSizes = {1000, 10000, 100000, 1000000, 10000000};
 
-    // Create a sorted vector from 1 to 1000
-    vector<int> arr1;
-    for (int i = 1; i <= 1000; ++i) {
-        arr1.push_back(i);
-    }
+    for (int size : inputSizes) {
+        // Create a sorted vector from 1 to size
+        vector<int> arr;
+        for (int i = 1; i <= size; ++i) {
+            arr.push_back(i);
+        }
 
-    // Perform binary search
-    int index = binarysearch(arr1, target);
+        // Target element (can be any element within the range)
+        int target = size / 2;
 
-    // Output the result
-    if (index != -1) {
-        cout << "The element " << target << " is at index " << index << endl;
-    } else {
-        cout << "The element " << target << " is not found in the array." << endl;
+        // Start measuring time
+        auto start = high_resolution_clock::now();
+
+        // Perform binary search
+        int index = binarysearch(arr, target);
+
+        // Stop measuring time
+        auto stop = high_resolution_clock::now();
+
+        // Calculate the duration
+        auto duration = duration_cast<milliseconds>(stop - start);
+
+        // Output the result
+        cout << "Input size: " << size << ", Time taken: " << duration.count() << " microseconds" << endl;
     }
 
     return 0;
